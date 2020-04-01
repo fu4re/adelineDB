e1d6dfd3220dc87754a68de12b9067e7a2d6d4af
fu4re <errozergame@gmail.com>
2020-04-01 20:23:59 +0300

    Add a cmd query

--- adelineDB.py
+++ adelineDB.py
@@ -2,5 +2,25 @@ import shelve
 
 db = shelve.open('make/.dbfile/aeData')
 
+fieldnames = ('name', 'age', 'job', 'pay') 
+maxfield = max(len(f) for f in fieldnames)
+
 for key in db:
     print(key, '=>\n', db[key].name, db[key].pay)
+
+#db = shelve.open('make/.dbfile/aeData')
+
+while True:
+    key = input('\nEnter a key => ') 
+
+    if not key: break
+
+    try:
+        record = db[key]
+
+    except:
+        print('No such key "%s"!' % key)
+
+    else:
+        for field in fieldnames:
+            print(field.ljust(maxfield), '=>', getattr(record, field))
